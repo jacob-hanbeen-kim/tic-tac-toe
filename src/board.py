35f75fbe31c227e07f469eb0eb6fc47cf3766b6c
Jacob Kim <jacobkim@jacobs-mbp.lan>
2020-08-08 03:21:26 -0400

    created the tic-tac-toe game with human player and random player

---
+++ src/board.py
@@ -0,0 +1,76 @@
+class Board:
+
+    BLANK = " "
+    PLAYER1_SYMBOL = "X"
+    PLAYER2_SYMBOL = "O"
+
+    def __init__(self, size=3):
+        self.size = size
+        print('Created board of size: {}'.format(self.size))
+        self.createGameBoard()
+
+    def createGameBoard(self):
+        self.board = [[Board.BLANK for i in range(0, self.size)] for j in range(0, self.size)]
+        self.possible_moves = []
+        for i in range(0, self.size):
+            for j in range(0, self.size):
+                self.possible_moves.append((i,j))
+
+        self.not_possible_moves = []
+
+    def get_possible_move(self):
+        return self.possible_moves
+
+    def apply_move(self, player):
+        move = player.move(self)
+        if move in self.possible_moves:
+            symbol = player.get_symbol()
+            name = player.get_name()
+
+            print("** {} placed '{}' on {} ** \n".format(name, symbol, move))
+
+            self.board[move[0]][move[1]] = symbol
+            self.possible_moves.remove(move)
+            
+            self.printBoard()
+            print("")
+
+            if self.__check_game_state__(move, symbol):
+                return True, player.get_name() + ' WIN!'                
+        else:
+            return True, 'Error. Invalid move!'
+
+        return False, ''
+
+    def __check_game_state__(self, move, symbol):
+        i = move[0]
+        j = move[1]
+
+        vertical = True
+        horizontal = True
+        diagonal_1 = i == j
+        diagonal_2 = i + j == self.size - 1
+        for s in range(self.size):
+            vertical = vertical and self.board[s][j] == symbol
+            horizontal = horizontal and self.board[i][s] == symbol
+            diagonal_1 = diagonal_1 and self.board[s][s] == symbol
+            diagonal_2 = diagonal_2 and self.board[s][self.size - 1] == symbol
+
+        return vertical or horizontal or diagonal_1 or diagonal_1
+
+
+    def printBoard(self):
+        out = '   '
+        for i in range(len(self.board[0])):
+            out += str(i) + ' '
+        out += '\n\r'
+
+        for i in range(len(self.board)):
+            out += str(i) + ' |'
+            for j in range(len(self.board[i])):
+                out += self.board[i][j] + '|'
+
+            if i != len(self.board) - 1:
+                out += '\n\r'
+
+        print(out)
